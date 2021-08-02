d17ac54a410a80ec980a2e895c00123fcf2d65b8
Ifthenpay <ricardocarvalho@ifthenpay.com>
2021-08-02 12:22:25 +0100

    fix bug changePaymentStatus in multibanco, mbway and payshop

--- opencart versão 3/system/library/ifthenpay/payments/data/MbwayChangePaymentStatus.php
+++ opencart versão 3/system/library/ifthenpay/payments/data/MbwayChangePaymentStatus.php
@@ -21,29 +21,31 @@ class MbwayChangePaymentStatus extends CheckPaymentStatusBase
     
     public function changePaymentStatus(): void
     {
-        $this->setGatewayDataBuilder();
-        $this->getPendingOrders();
-        if (!empty($this->pendingOrders)) {
-            foreach ($this->pendingOrders as $pendingOrder) {
-                $mbwayPayment = $this->ifthenpayController->model_extension_payment_mbway->getPaymentByOrderId($pendingOrder['order_id'])->row;
-                if (!empty($mbwayPayment)) {
-                    $this->gatewayDataBuilder->setIdPedido($mbwayPayment['id_transacao']);
-                    if ($this->paymentStatus->setData($this->gatewayDataBuilder)->getPaymentStatus()) {
-                        $this->ifthenpayController->model_extension_payment_mbway->updatePaymentStatus(
-                            $mbwayPayment['id_ifthenpay_mbway'], 
-                            'paid'
-                        );
-                        $catalogChangeOrderStatusEndpoint = $this->ifthenpayController->config->get('config_secure') ? rtrim(HTTP_CATALOG, '/') : rtrim(HTTPS_CATALOG, '/') . '/index.php?route=extension/payment/mbway/changeOrderStatusFromWebservice';
-                        $this->webservice->getRequest(
-                            $catalogChangeOrderStatusEndpoint,
-                            [
-                            'order_id' => $pendingOrder['order_id'],
-                            ],
-                            false
-                        );
+        if ($this->ifthenpayController->config->get('payment_mbway_mbwayKey')) {
+            $this->setGatewayDataBuilder();
+            $this->getPendingOrders();
+            if (!empty($this->pendingOrders)) {
+                foreach ($this->pendingOrders as $pendingOrder) {
+                    $mbwayPayment = $this->ifthenpayController->model_extension_payment_mbway->getPaymentByOrderId($pendingOrder['order_id'])->row;
+                    if (!empty($mbwayPayment)) {
+                        $this->gatewayDataBuilder->setIdPedido($mbwayPayment['id_transacao']);
+                        if ($this->paymentStatus->setData($this->gatewayDataBuilder)->getPaymentStatus()) {
+                            $this->ifthenpayController->model_extension_payment_mbway->updatePaymentStatus(
+                                $mbwayPayment['id_ifthenpay_mbway'], 
+                                'paid'
+                            );
+                            $catalogChangeOrderStatusEndpoint = $this->ifthenpayController->config->get('config_secure') ? rtrim(HTTP_CATALOG, '/') : rtrim(HTTPS_CATALOG, '/') . '/index.php?route=extension/payment/mbway/changeOrderStatusFromWebservice';
+                            $this->webservice->getRequest(
+                                $catalogChangeOrderStatusEndpoint,
+                                [
+                                'order_id' => $pendingOrder['order_id'],
+                                ],
+                                false
+                            );
+                        }
                     }
+                    
                 }
-                
             }
         }
     }
--- opencart versão 3/system/library/ifthenpay/payments/data/PayshopChangePaymentStatus.php
+++ opencart versão 3/system/library/ifthenpay/payments/data/PayshopChangePaymentStatus.php
@@ -22,29 +22,32 @@ class PayshopChangePaymentStatus extends CheckPaymentStatusBase
     
     public function changePaymentStatus(): void
     {
-        $this->setGatewayDataBuilder();
-        $this->getPendingOrders();
-        if (!empty($this->pendingOrders)) {
-            foreach ($this->pendingOrders as $pendingOrder) {
-                $payshopPayment = $this->ifthenpayController->model_extension_payment_payshop->getPaymentByOrderId($pendingOrder['order_id'])->row;
-                if (!empty($payshopPayment)) {
-                    $this->gatewayDataBuilder->setReferencia($payshopPayment['referencia']);
-                    if ($this->paymentStatus->setData($this->gatewayDataBuilder)->getPaymentStatus()) {
-                        $this->ifthenpayController->model_extension_payment_payshop->updatePaymentStatus(
-                            $payshopPayment['id_ifthenpay_payshop'], 
-                            'paid'
-                        );
-                        $catalogChangeOrderStatusEndpoint = $this->ifthenpayController->config->get('config_secure') ? rtrim(HTTP_CATALOG, '/') : rtrim(HTTPS_CATALOG, '/') . '/index.php?route=extension/payment/payshop/changeOrderStatusFromWebservice';
-                        $this->webservice->postRequest(
-                            $catalogChangeOrderStatusEndpoint,
-                            [
-                            'order_id' => $pendingOrder['order_id'],
-                            ],
-                            false
-                        );
+        if ($this->ifthenpayController->config->get('payment_payshop_backofficeKey') && $this->ifthenpayController->config->get('payment_payshop_payshopKey')) {
+            $this->setGatewayDataBuilder();
+            $this->getPendingOrders();
+            if (!empty($this->pendingOrders)) {
+                foreach ($this->pendingOrders as $pendingOrder) {
+                    $payshopPayment = $this->ifthenpayController->model_extension_payment_payshop->getPaymentByOrderId($pendingOrder['order_id'])->row;
+                    if (!empty($payshopPayment)) {
+                        $this->gatewayDataBuilder->setReferencia($payshopPayment['referencia']);
+                        if ($this->paymentStatus->setData($this->gatewayDataBuilder)->getPaymentStatus()) {
+                            $this->ifthenpayController->model_extension_payment_payshop->updatePaymentStatus(
+                                $payshopPayment['id_ifthenpay_payshop'], 
+                                'paid'
+                            );
+                            $catalogChangeOrderStatusEndpoint = $this->ifthenpayController->config->get('config_secure') ? rtrim(HTTP_CATALOG, '/') : rtrim(HTTPS_CATALOG, '/') . '/index.php?route=extension/payment/payshop/changeOrderStatusFromWebservice';
+                            $this->webservice->postRequest(
+                                $catalogChangeOrderStatusEndpoint,
+                                [
+                                'order_id' => $pendingOrder['order_id'],
+                                ],
+                                false
+                            );
+                        }
                     }
                 }
             }
         }
+        
     }
 }
